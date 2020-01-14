b49122703ac97fa33df508b5a68b360f71f1925e
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-01-14 14:18:17 +0900

    追加課題１、２を提出

--- js_exam/scoring.js
+++ js_exam/scoring.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   let subject_quantity; //全教科の科目数
   let ave; //全教科の平均点
   let rank; //ランク
-  let judge; //合格・不合格
+  let result; //合格・不合格
 
   function score_indicate(){
     // このような記述をすることで、subject_pointsという変数の中に
@@ -16,11 +16,15 @@ $(document).ready(function(){
                           Number($('#society').val())
                           ];
     // さらにこのような記述をすることで、「合計点：」となっている右の部分に合計点が出力される
+/*
     let sum = subject_points[0];
     sum = sum + subject_points[1];
     sum = sum + subject_points[2];
     sum = sum + subject_points[3];
     sum = sum + subject_points[4];
+*/
+//追加課題１
+    let sum =subject_points.reduce(function(a,b){ return a + b; });
     $("#sum_indicate").text(sum);
 
     // ここに、上記を参考にして平均点を出力する処理を書き込む
@@ -46,19 +50,21 @@ $(document).ready(function(){
 
   function get_pass_or_failure(){
     // ここに、全ての教科が60点以上なら"合格"の文字列、一つでも60点未満の教科があったら"不合格"の文字列を出す処理を書き込む
-    judge = "合格";
+    result = "合格";
     for (var i = 0; i < subject_quantity; i++) {
       if(subject_points[i] < 60){
-        judge = "不合格";
+        result = "不合格";
       }
     }
-    return judge;
+    return result;
   }
 
   function judgement(){
     // ここに、「最終ジャッジ」のボタンを押したら「あなたの成績はAです。合格です」といった内容を出力する処理を書き込む
     // 下記の記述をすることで、「最終ジャッジ」のボタンを押すと「あなたの成績は（ここに「ランク」の値を入れる）です。（ここに「判定」の値を入れる）です」という文字の入った水色のフキダシが出力される処理が実装される。
-    $('#declaration').append(`<label id="alert-indicate" class="alert alert-info">あなたの成績は${rank}です。${judge}です</label>`);
+//追加課題２
+    $('#alert-indicate').remove();
+    $('#declaration').append(`<label id="alert-indicate" class="alert alert-info">あなたの成績は${rank}です。${result}です</label>`);
   };
 
   $('#national_language, #english, #mathematics, #science, #society').change(function() {
